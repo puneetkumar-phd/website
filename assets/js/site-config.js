defea6b16fd43b9ce17db95c02777293d64e040d
Dr. Puneet Kumar <127096280+puneetkumar-phd@users.noreply.github.com>
2026-07-29 15:27:30 +0400

    Delete assets directory

--- assets/js/site-config.js
+++
@@ -1,7 +0,0 @@
-window.SITE_CONFIG = {
-  // Create a free GoatCounter site and replace the value below, e.g. "puneetkumar".
-  goatCounterCode: "puneet",
-
-  // Optional GitHub profile. The GitHub link stays hidden until this is replaced.
-  githubProfile: "https://puneetkumar-phd.github.io/website/"
-};
