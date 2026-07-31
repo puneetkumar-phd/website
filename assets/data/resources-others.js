1fa7d16485716edc946acb9c5564c1885a93f48b
Dr. Puneet Kumar <127096280+puneetkumar-phd@users.noreply.github.com>
2026-07-31 11:52:49 +0400

    Delete assets/data directory

--- assets/data/resources-others.js
+++
@@ -1,4 +0,0 @@
-/* Use this section for valuable resources that do not fit the six main sections. */
-window.RESOURCE_LIBRARY = window.RESOURCE_LIBRARY || {};
-window.RESOURCE_LIBRARY["others"] = (window.RESOURCE_LIBRARY["others"] || []).concat([
-]);
